d639fb993b108f360f798c25ba1ee6a7dc489614
Jeremy Edbert Widjaja <widjajajeremy61@gmail.com>
2022-01-10 14:01:12 +0700

    update navbar + landing

--- resources/views/partials/user-navbar.blade.php
+++ resources/views/partials/user-navbar.blade.php
@@ -1,22 +1,15 @@
 <div class="header-top-bar">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-6">
-                <ul class="top-bar-info list-inline-item pl-0 mb-0">
-                    <li class="list-inline-item"><a href="mailto:support@gmail.com"><i
-                                class="icofont-support-faq mr-2"></i>support@novena.com</a></li>
-                    <li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Address Ta-134/A, New York, USA
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-6">
-                <div class="text-lg-right top-right-bar mt-2 mt-lg-0">
-                    <a href="tel:+23-345-67890">
-                        <span>Call Now : </span>
-                        <span class="h4">823-4565-13456</span>
-                    </a>
-                </div>
-            </div>
+            <ul class="top-bar-info list-inline-item pl-0 mb-0">
+                <li class="list-inline-item mr-4"><i class="icofont-email mr-2"></i> simentel@semarangkota.go.id</li>
+                <li class="list-inline-item mr-4"><i class="icofont-location-pin mr-2"></i>Jl. Pemuda No.148, Semarang 50132
+                </li>
+                <li class="list-inline-item mr-4"><i class="icofont-ui-call mr-2"></i>
+                    (024) 3513366
+                </li>
+
+            </ul>
         </div>
     </div>
 </div>
@@ -33,26 +26,15 @@
 
         <div class="collapse navbar-collapse" id="navbarmain">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="service.html">Services</a></li>
+                <li class="nav-item active"><a class="nav-link" href="index.html">Beranda</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">Pendaftaran</a></li>
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">Department <i class="icofont-thin-down"></i></a>
+                        aria-haspopup="true" aria-expanded="false">Peta<i class="icofont-thin-down"></i></a>
                     <ul class="dropdown-menu" aria-labelledby="dropdown02">
-                        <li><a class="dropdown-item" href="department.html">Departments</a></li>
-                        <li><a class="dropdown-item" href="department-single.html">Department Single</a></li>
-
-                        <li class="dropdown dropdown-submenu dropright">
-                            <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
-
-                            <ul class="dropdown-menu" aria-labelledby="dropdown0301">
-                                <li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
-                                <li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
-                            </ul>
-                        </li>
+                        <li><a class="dropdown-item" href="department.html">Peta Menara</a></li>
+                        <li><a class="dropdown-item" href="department-single.html">Peta Microcell</a></li>
                     </ul>
                 </li>
 
@@ -73,15 +55,6 @@
                                 <li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
                             </ul>
                         </li>
-                    </ul>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown05">
-                        <li><a class="dropdown-item" href="blog-sidebar.html">Blog with Sidebar</a></li>
-                        <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
                     </ul>
                 </li>
                 <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
