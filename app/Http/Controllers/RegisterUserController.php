fac48240c5fb856454fabdeecbed0d3a0d3d3f54
gregoriuswilly <grewilly2001@gmail.com>
2022-01-18 12:36:48 +0700

    halaman register

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterUserController extends Controller
 {
-    
+  public function index()
+  {
+      return view('user.register', [
+          'active' => 'register',
+      ]);
+  }
 }
