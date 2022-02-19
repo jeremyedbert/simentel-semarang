44d67fa93d85f44c3be69a3b0dbb0a20f6e2d075
Jeremy Edbert Widjaja <widjajajeremy61@gmail.com>
2022-02-20 00:50:35 +0700

    menambah counter di halaman welcome user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tower;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('user.welcome');
+        // return dd(Tower::find(1)->select(DB::raw('count(*)'))->get());
+        return view('user.welcome', [
+            'macro' => Tower::where('tipe_menara_id', 1)->where('acc_date', '!=', NULL)->count(),
+            'micro' => Tower::where('tipe_menara_id', 2)->where('acc_date', '!=', NULL)->count(),
+        ]);
     }
 }
