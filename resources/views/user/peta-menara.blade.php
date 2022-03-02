7d33534704f5a275231c7d635a567260fd91c74a
gregoriuswilly <grewilly2001@gmail.com>
2022-03-02 08:49:59 +0700

    edit notif + nyoba green zone di maps

--- resources/views/user/peta-menara.blade.php
+++ resources/views/user/peta-menara.blade.php
@@ -78,30 +78,30 @@
             google.maps.event.addDomListener(window, 'load', initialize);
         }
 
-        function initMap() {
-          // Create the map.
-          const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 4,
-            center: { lat: 37.09, lng: -95.712 },
-            mapTypeId: "terrain",
-          });
+        // function initMap() {
+        //   // Create the map.
+        //   const map = new google.maps.Map(document.getElementById("map"), {
+        //     zoom: 4,
+        //     center: { lat: 37.09, lng: -95.712 },
+        //     mapTypeId: "terrain",
+        //   });
 
-          // Construct the circle for each value in citymap.
-          // Note: We scale the area of the circle based on the population.
-          for (const city in citymap) {
-            // Add the circle for this city to the map.
-            const cityCircle = new google.maps.Circle({
-              strokeColor: "#FF0000",
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: "#FF0000",
-              fillOpacity: 0.35,
-              map,
-              center: citymap[city].center,
-              radius: Math.sqrt(citymap[city].population) * 100,
-            });
-          }
-        }
+        //   // Construct the circle for each value in citymap.
+        //   // Note: We scale the area of the circle based on the population.
+        //   for (const city in citymap) {
+        //     // Add the circle for this city to the map.
+        //     const cityCircle = new google.maps.Circle({
+        //       strokeColor: "#FF0000",
+        //       strokeOpacity: 0.8,
+        //       strokeWeight: 2,
+        //       fillColor: "#FF0000",
+        //       fillOpacity: 0.35,
+        //       map,
+        //       center: citymap[city].center,
+        //       radius: Math.sqrt(citymap[city].population) * 100,
+        //     });
+        //   }
+        // }
 
         function theContent(marker, tower) {
             let kelurahan = @json($kelurahan);
