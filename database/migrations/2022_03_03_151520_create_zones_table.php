96029a8cffab41a27d37471824f5051f56b96ded
gregoriuswilly <grewilly2001@gmail.com>
2022-03-05 10:32:40 +0700

    migration zona

--- database/migrations/2022_03_03_151520_create_zones_table.php
+++ database/migrations/2022_03_03_151520_create_zones_table.php
@@ -15,6 +15,14 @@ class CreateZonesTable extends Migration
     {
         Schema::create('zones', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+
+            $table->unsignedBigInteger('kecamatan_id');
+            $table->foreign('kecamatan_id')->references('id')->on('kecamatans');
+            
+            $table->decimal('latitude',10,7);
+            $table->decimal('longitude',10,7);
+            $table->integer('radius');
             $table->timestamps();
         });
     }
